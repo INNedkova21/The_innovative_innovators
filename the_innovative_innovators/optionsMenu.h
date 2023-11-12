a242abad8d7f8fc4b96ac679970f0662e1d9dd0d
BNDukovXXI <bndukov21@codingburgas.bg>
2023-11-12 23:19:21 +0200

    add update options menu

--- the_innovative_innovators/optionsMenu.h
+++ the_innovative_innovators/optionsMenu.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <iostream>
+#include <raylib.h>
+#include "Textures.h"
+#include "tchar.h"
+#include "mainMenu.h"
+using namespace std;
+void optionsMenu()
+{
+	BeginDrawing();
+	ClearBackground(WHITE);
+	DrawTexture(optionsBackground, 0, 0, WHITE);
+	DrawTexture(Fullscreen, 820, 350, WHITE);
+	DrawTexture(SourceCode, 820, 550, WHITE);
+	DrawTexture(BackToMenu, 820, 750, WHITE);
+	DrawTextureV(Selector, selectorPos, WHITE);
+	EndDrawing();
+}
