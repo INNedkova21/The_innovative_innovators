cb7ba85faf3bb1af2d9b0a60cbdc25c16b986856
BNDukovXXI <bndukov21@codingburgas.bg>
2023-11-12 23:12:48 +0200

    update char header

--- the_innovative_innovators/char.h
+++ the_innovative_innovators/char.h
@@ -3,15 +3,22 @@
 #include <iostream>
 using namespace std;
 
-struct Character {
-	bool isAlive = true;
+Vector2 charPos = { 1920.0f / 2, 1080.0f / 2 };
+Vector2 bulletPos = { charPos.x, charPos.y };
+struct Character // character
+{
 	float speed = 3;
 	float health = 100;
-	float atkSpeed = 1.00;
-	float dmg = 1.00;
-	float hRegen = 0.50;
+	float atkSpeed = 0.8;
+	float dmg = 150;
+	float exp = 0;
+	float baseHealth = 100;
+	float baseAtkSpeed = 0.8;
+	float baseDmg = 150;
+
 };
-void cMovement(Character c)
+
+void cMovement(Character c) // character movement
 {
 	if (IsKeyDown(KEY_RIGHT) && charPos.x < 1810) charPos.x += c.speed;
 	if (IsKeyDown(KEY_LEFT) && charPos.x > 105)charPos.x -= c.speed;
